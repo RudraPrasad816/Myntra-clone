852f0ae40297f0d95c28064d33f43a9f4c9b3cc1
Rudra Prasad <111743764+RudraPrasad816@users.noreply.github.com>
2023-08-14 02:45:19 +0530

    'initial'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import { Header } from './components';
+import { Home, ProductsPage, CartPage, LoginPage, RegisterPage, Error, ProductPage } from './pages';
+import data from './data/data';
+import { auth } from './firebase';
+
+export const DataContext = React.createContext(data);
 
 function App() {
+
+  const [newData, setNewData] = useState(data);
+
+  const setNewDataClick = (filtereddata) => {
+    setNewData(filtereddata);
+  }
+  const [user, setUser] = useState({ name: "", email: "" })
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      setUser(user);
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <DataContext.Provider value={{ newData, setNewDataClick, user }} >
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/cart' element={<CartPage />} />
+            <Route path="/product/:id" element={<ProductPage />} />
+            <Route path="/products/:page" element={<ProductsPage />} />
+            <Route path="/products/:page" element={<ProductsPage />} />
+            <Route path='/create-new-account' element={<RegisterPage />} />
+            <Route path='/login' element={<LoginPage />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </BrowserRouter>
+      </DataContext.Provider>
+    </>
+  )
 }
 
 export default App;
